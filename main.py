5fd2f3bc92ff29bf8938c34c43de397a040dd6ac
NateWebber <nwebber@u.rochester.edu>
2021-02-28 18:40:52 -0500

    take 3 please work

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ KVUri = f"https://{keyVaultName}.vault.azure.net"
 secretName = "ConglomerbotKey"
 
 credential = DefaultAzureCredential()
-client = SecretClient(vault_url=KVUri, credential=credential)
-retrieved_secret = client.get_secret(secretName)
+secretClient = SecretClient(vault_url=KVUri, credential=credential)
+retrieved_secret = secretClient.get_secret(secretName)
 
 print(
     f"The value of secret '{secretName}' in '{keyVaultName}' is: '{retrieved_secret.value}'")
 
-discordClient = discord.Client()
+client = discord.Client()
 
 
-@discordClient.event
+@client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@discordClient.event
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
@@ -34,4 +34,4 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 
-discordClient.run(retrieved_secret.value)  # secret key goes here
+client.run(retrieved_secret.value)  # secret key goes here
