95b54135a1351ccd5759c9504a98b165fde332d4
Nate Webber <nwebber@u.rochester.edu>
2021-03-01 02:56:17 +0000

    added return statements

--- main.py
+++ main.py
@@ -54,8 +54,10 @@ async def rr_challenge(ctx, challengee: discord.User):
     if (isinstance(challengee, discord.Member)):
         rr.start_challenge(ctx.author, challengee)
         await ctx.send("{0}, you have been challenged to a game of Russian Roulette by {1}! Use $rr_accept to accept!".format(challengee.mention, ctx.author.mention))
+        return
     else:
         await ctx.send("You need to mention a valid opponent!")
+        return
 
 #rr_cancel: cancels the outstanding russian roulette challenge, provided there was one, and the author is the issuer
 @client.command()
@@ -65,10 +67,13 @@ async def rr_cancel(ctx):
         if (rr.current_challenger == ctx.author):
             rr.cancel()
             await ctx.send("Cancelled the active challenge.")
+            return
         else:
             await ctx.send("You were not the issuer of the challenge!")
+            return
     else:
         await ctx.send("No active challenge to cancel!")
+        return
 
 #rr_accept: accepts the current russian roulette challenge, provided the author is the challengee
 @client.command()
@@ -77,10 +82,13 @@ async def rr_accept(ctx):
         if (rr.current_challengee == ctx.author):
             #rr.play() #commented out bc it'll crash when there's no logic
             await ctx.send("Starting a game between {0} and {1}!".format(challenger.mention, ctx.author.mention))
+            return
         else:
             await ctx.send("You were not the person challenged!")
+            return
     else:
         await ctx.send("No active challenge to accept!")
+        return
 
 #rr_decline: declines the current russian roulette challenge, provided the author is the challengee
 @client.command()
@@ -89,10 +97,13 @@ async def rr_decline(ctx):
         if (rr.current_challengee == ctx.author):
             await ctx.send("In a display of great cowardice, {0} has declined {1}'s challenge!".format(challengee.mention, challenger.mention))
             rr.cancel()
+            return
         else:
             await ctx.send("You were not the person challenged!")
+            return
     else:
         await ctx.send("No active challenge to cancel!")
+        return
 
 
 client.run(retrieved_secret.value)  # secret key goes here
