6f6bd60bebb4aa00796beff39f36f9ebaf8f6d9f
NateWebber <nwebber@u.rochester.edu>
2021-02-28 19:36:31 -0500

    what is the type of challengee?

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ async def hello(ctx):
 
 @client.command()
 async def rr_start(ctx, challengee):
+    print(type(challengee))
     if (rr.currently_playing):
         await ctx.send("Sorry, a game is already being played.")
         return
